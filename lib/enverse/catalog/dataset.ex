85a22cfcb67ef032a503a34e8b585e0d2770c0a2
Andreas Loo <andreas@gaup.io>
2024-06-10 15:16:51 +0200

    Fix indentation

--- lib/enverse/catalog/dataset.ex
+++ lib/enverse/catalog/dataset.ex
@@ -2,54 +2,54 @@ defmodule Enverse.Catalog.Dataset do
   use Ash.Resource,
     data_layer: AshPostgres.DataLayer
 
-    postgres do
-      table "datasets"
-      repo Enverse.Repo
+  postgres do
+    table "datasets"
+    repo Enverse.Repo
+  end
+
+  code_interface do
+    define_for Enverse.Catalog
+    define :create, action: :create
+    define :read_all, action: :read
+    define :update, action: :update
+    define :destroy, action: :destroy
+    define :get_by_id, args: [:id], action: :by_id
+  end
+
+  actions do
+    defaults [:create, :read, :update, :destroy]
+
+    read :by_id do
+      argument :id, :uuid, allow_nil?: false
+      get? true
+      filter expr(id == ^arg(:id))
+    end
+  end
+
+  attributes do
+    uuid_primary_key :id
+    create_timestamp :created_at
+    update_timestamp :updated_at
+
+    attribute :title, :string do
+      allow_nil? false
     end
 
-    code_interface do
-      define_for Enverse.Catalog
-      define :create, action: :create
-      define :read_all, action: :read
-      define :update, action: :update
-      define :destroy, action: :destroy
-      define :get_by_id, args: [:id], action: :by_id
+    attribute :description, :string do
+      allow_nil? false
     end
 
-    actions do
-      defaults [:create, :read, :update, :destroy]
-
-      read :by_id do
-        argument :id, :uuid, allow_nil?: false
-        get? true
-        filter expr(id == ^arg(:id))
-      end
+    attribute :metdata, :map do
+      allow_nil? false
+      default %{}
     end
 
-    attributes do
-      uuid_primary_key :id
-      create_timestamp :created_at
-      update_timestamp :updated_at
-
-      attribute :title, :string do
-        allow_nil? false
-      end
-
-      attribute :description, :string do
-        allow_nil? false
-      end
-
-      attribute :metdata, :map do
-        allow_nil? false
-        default %{}
-      end
-
-      attribute :descriptor, Enverse.Catalog.Descriptors.Dataset do
-        allow_nil? false
-      end
+    attribute :descriptor, Enverse.Catalog.Descriptors.Dataset do
+      allow_nil? false
     end
+  end
 
-    relationships do
-      has_many :record, Enverse.Catalog.Record
-    end
+  relationships do
+    has_many :record, Enverse.Catalog.Record
+  end
 end
--- lib/enverse/catalog/record.ex
+++ lib/enverse/catalog/record.ex
@@ -2,56 +2,56 @@ defmodule Enverse.Catalog.Record do
   use Ash.Resource,
     data_layer: AshPostgres.DataLayer
 
-    postgres do
-      table "records"
-      repo Enverse.Repo
+  postgres do
+    table "records"
+    repo Enverse.Repo
+  end
+
+  code_interface do
+    define_for Enverse.Catalog
+    define :create, action: :create
+    define :read_all, action: :read
+    define :update, action: :update
+    define :destroy, action: :destroy
+    define :get_by_id, args: [:id], action: :by_id
+  end
+
+  actions do
+    defaults [:create, :read, :update, :destroy]
+
+    read :by_id do
+      argument :id, :uuid, allow_nil?: false
+      get? true
+      filter expr(id == ^arg(:id))
     end
 
-    code_interface do
-      define_for Enverse.Catalog
-      define :create, action: :create
-      define :read_all, action: :read
-      define :update, action: :update
-      define :destroy, action: :destroy
-      define :get_by_id, args: [:id], action: :by_id
+    create do
+      accept([:variables, :metdata, :dataset_id])
+    end
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :latitude, :float
+    attribute :longitude, :float
+    attribute :elevation, :float
+    attribute :time, :datetime
+
+    attribute :variables, :map do
+      allow_nil? false
+      default %{}
     end
 
-    actions do
-      defaults [:create, :read, :update, :destroy]
-
-      read :by_id do
-        argument :id, :uuid, allow_nil?: false
-        get? true
-        filter expr(id == ^arg(:id))
-      end
-
-      create do
-        accept([:variables, :metdata, :dataset_id])
-      end
+    attribute :metdata, :map do
+      allow_nil? false
+      default %{}
     end
+  end
 
-    attributes do
-      uuid_primary_key :id
-
-      attribute :latitude, :float
-      attribute :longitude, :float
-      attribute :elevation, :float
-      attribute :time, :datetime
-
-      attribute :variables, :map do
-        allow_nil? false
-        default %{}
-      end
-
-      attribute :metdata, :map do
-        allow_nil? false
-        default %{}
-      end
+  relationships do
+    belongs_to :dataset, Enverse.Catalog.Dataset do
+      attribute_writable? true
     end
-
-    relationships do
-      belongs_to :dataset, Enverse.Catalog.Dataset do
-        attribute_writable? true
-      end
-    end
+  end
 end
